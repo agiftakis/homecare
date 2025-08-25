17f80b578e99fdded45e74c5b3fc87f1d0e2a5df
AndyGis <andrewgis43@gmail.com>
2025-08-25 13:40:32 -0400

    updated client.php file

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone_number',
+        'address',
+        'date_of_birth',
+        'care_plan',
+    ];
 }
