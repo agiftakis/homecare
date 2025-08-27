8c08a1cb950b80693f90790d25428cfa0f29113b
AndyGis <andrewgis43@gmail.com>
2025-08-27 13:05:36 -0400

    updated views/caregivers and clients index.blade.php files for cometics 099

--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -12,45 +12,44 @@
                     <form method="POST" action="{{ route('clients.store') }}" enctype="multipart/form-data">
                         @csrf
 
-                        {{-- We'll use a grid for a better layout --}}
                         <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
 
                             <!-- First Name -->
                             <div>
                                 <x-input-label for="first_name" :value="__('First Name')" />
-                                <x-text-input id="first_name" class="block mt-1 w-full" type="text" name="first_name" :value="old('first_name')" required autofocus />
+                                <x-text-input id="first_name" class="block mt-1 w-full" type="text" name="first_name" :value="old('first_name')" required autofocus autocomplete="given-name" />
                                 <x-input-error :messages="$errors->get('first_name')" class="mt-2" />
                             </div>
 
                             <!-- Last Name -->
                             <div>
                                 <x-input-label for="last_name" :value="__('Last Name')" />
-                                <x-text-input id="last_name" class="block mt-1 w-full" type="text" name="last_name" :value="old('last_name')" required />
+                                <x-text-input id="last_name" class="block mt-1 w-full" type="text" name="last_name" :value="old('last_name')" required autocomplete="family-name" />
                                 <x-input-error :messages="$errors->get('last_name')" class="mt-2" />
                             </div>
 
                             <!-- Email Address -->
                             <div>
                                 <x-input-label for="email" :value="__('Email')" />
-                                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="email" />
                                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
                             </div>
 
                             <!-- Phone Number -->
                             <div>
                                 <x-input-label for="phone_number" :value="__('Phone Number')" />
-                                <x-text-input id="phone_number" class="block mt-1 w-full" type="text" name="phone_number" :value="old('phone_number')" required />
+                                <x-text-input id="phone_number" class="block mt-1 w-full" type="text" name="phone_number" :value="old('phone_number')" required autocomplete="tel" />
                                 <x-input-error :messages="$errors->get('phone_number')" class="mt-2" />
                             </div>
                             
                             <!-- Date of Birth -->
                             <div>
                                 <x-input-label for="date_of_birth" :value="__('Date of Birth')" />
-                                <x-text-input id="date_of_birth" class="block mt-1 w-full" type="date" name="date_of_birth" :value="old('date_of_birth')" required />
+                                <x-text-input id="date_of_birth" class="block mt-1 w-full" type="date" name="date_of_birth" :value="old('date_of_birth')" required autocomplete="bday" />
                                 <x-input-error :messages="$errors->get('date_of_birth')" class="mt-2" />
                             </div>
 
-                             <!-- Profile Picture -->
+                            <!-- Profile Picture -->
                             <div>
                                 <x-input-label for="profile_picture" :value="__('Profile Picture (Optional)')" />
                                 <input id="profile_picture" name="profile_picture" type="file" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 mt-1">
@@ -60,7 +59,7 @@
                             <!-- Address (Spans full width) -->
                             <div class="md:col-span-2">
                                 <x-input-label for="address" :value="__('Address')" />
-                                <textarea id="address" name="address" class="block mt-1 w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" required>{{ old('address') }}</textarea>
+                                <textarea id="address" name="address" class="block mt-1 w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" required autocomplete="street-address">{{ old('address') }}</textarea>
                                 <x-input-error :messages="$errors->get('address')" class="mt-2" />
                             </div>
 
