a659a6c0ec55ebae9b35360f99bc1fb0ddf49be5
AndyGis <andrewgis43@gmail.com>
2025-08-29 09:57:08 -0400

    created new navigation link for schedualing

---
+++ resources/views/schedule/index.blade.php
@@ -0,0 +1,20 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Schedule') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <h3 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-4">
+                        Scheduling Dashboard
+                    </h3>
+                    <p>The calendar and shift creation form will go here.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
