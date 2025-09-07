b95a0fcc1d67151b0a4c4fb97fcf1d0b7717506b
AndyGis <andrewgis43@gmail.com>
2025-09-07 10:05:04 -0400

    Started Visit Verification System 01

---
+++ app/Models/Visit.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Visit extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'shift_id',
+        'agency_id',
+        'clock_in_time',
+        'clock_out_time',
+        'signature_path',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'clock_in_time' => 'datetime',
+        'clock_out_time' => 'datetime',
+    ];
+
+    /**
+     * Get the shift that this visit belongs to.
+     */
+    public function shift()
+    {
+        return $this->belongsTo(Shift::class);
+    }
+
+    /**
+     * Get the agency that this visit belongs to.
+     */
+    public function agency()
+    {
+        return $this->belongsTo(Agency::class);
+    }
+}
