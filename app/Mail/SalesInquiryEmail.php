82e051594fa217497281334628b87e949dc9bb0d
AndyGis <andrewgis43@gmail.com>
2025-11-13 12:49:25 -0500

    New Contact Sales Form integration02

---
+++ app/Mail/SalesInquiryEmail.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SalesInquiryEmail extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * The inquiry data from the contact form.
+     *
+     * @var array
+     */
+    public $inquiryData;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param  array  $inquiryData
+     * @return void
+     */
+    public function __construct(array $inquiryData)
+    {
+        $this->inquiryData = $inquiryData;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            from: 'no-reply@vitalink.com', // You can change this 'from' address
+            subject: 'New VitaLink Agency Inquiry from: ' . $this->inquiryData['agency_name'],
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        // We will create this view in the next step
+        return new Content(
+            view: 'emails.sales-inquiry', 
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
